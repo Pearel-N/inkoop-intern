05bff79897c3385dfb94d0c1cfd788cb7b7d23f2
Pearel Nazareth <pearel@outlook.com>
2019-11-07 19:57:21 +0530

    static folder

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 
  var app = express();
 
+ app.use(express.static('static'));
+
  app.use(bodyParser.urlencoded({ extended: true}));
 
  app.get('/', function(req, res){
